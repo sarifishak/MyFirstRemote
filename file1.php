a227c2a4a427795ff54ae242573343d09fb5fd2a
Sarif Ishak <sarif@persistent.com>
2021-11-04 14:30:02 +0800

    Bugs456 version 1

--- file1.php
+++ file1.php
@@ -3,3 +3,5 @@
 file 1...... version 1
 
 file 1... ...ada bugs... version 2
+
+Update file 1 - for bugs456
--- file2.php
+++ file2.php
@@ -3,6 +3,8 @@
 file 2...... version 1
 
 file 2 changes ... to version 4
-=======
+
 
 file 2 .. ada bugs lagi ..version  3
+
+Update file 2 - for bugs456
