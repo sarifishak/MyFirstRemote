00fb8e72959078b1fe1dffbb7c4d0fed3e273478
Sarif Ishak <sarif@persistent.com>
2021-11-04 12:54:42 +0800

    create bugs again

--- file1.php
+++ file1.php
@@ -2,6 +2,6 @@
 
 file 1...... version 1
 
-file 1... ...ada bugs... version 2
+add here....introduce the bugs
 
-file 1 .. change for bug789
+file 1... ...ada bugs... version 2
--- file2.php
+++ file2.php
@@ -6,3 +6,7 @@ file 2 changes ... to version 4
 =======
 
 file 2 .. ada bugs lagi ..version  3
+
+
+
+add this line ... bug 2
