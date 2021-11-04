f959fdcccd36885f24a22f27aafa9822f1aae41b
Sarif Ishak <sarif@persistent.com>
2021-11-04 12:55:33 +0800

    Revert "create bugs again"
    This reverts commit c3438b061496fa0507bcf20b33aeb464bf19e789.

--- file1.php
+++ file1.php
@@ -2,6 +2,4 @@
 
 file 1...... version 1
 
-add here....introduce the bugs
-
 file 1... ...ada bugs... version 2
--- file2.php
+++ file2.php
@@ -6,7 +6,3 @@ file 2 changes ... to version 4
 =======
 
 file 2 .. ada bugs lagi ..version  3
-
-
-
-add this line ... bug 2
